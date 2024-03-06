49622e3bc5ba32addaf5feb447b7507bf132458c
Oluwaseyi Oyadeyi <oyasey83@gmai.com>
2024-03-06 09:15:41 -0700

    updated commit

--- main.tf
+++ main.tf
@@ -13,10 +13,10 @@ resource "azurerm_virtual_network" "testvn" {
 }
 
 resource "azurerm_availability_set" "azset" {
-  name                = "myAvailabilitySet"
-  resource_group_name = azurerm_resource_group.testrg.name
-  location            = azurerm_resource_group.testrg.location
-  platform_fault_domain_count = 2 # Specify the number of fault domains (availability zones)
+  name                         = "myAvailabilitySet"
+  resource_group_name          = azurerm_resource_group.testrg.name
+  location                     = azurerm_resource_group.testrg.location
+  platform_fault_domain_count  = 2 # Specify the number of fault domains (availability zones)
   platform_update_domain_count = 2
 
   # Additional settings can be configured here if needed
@@ -25,7 +25,7 @@ resource "azurerm_availability_set" "azset" {
 resource "azurerm_subnet" "public_subnet_1" {
   name                 = "public_subnet_1"
   resource_group_name  = azurerm_resource_group.testrg.name
-  virtual_network_name = azurerm_virtual_network.testvn.name 
+  virtual_network_name = azurerm_virtual_network.testvn.name
   address_prefixes     = ["10.0.1.0/24"]
 }
 
@@ -50,3 +50,86 @@ resource "azurerm_subnet" "private_subnet_2" {
   address_prefixes     = ["10.0.4.0/24"]
 }
 
+# Security Group
+resource "azurerm_network_security_group" "testSG" {
+  name                = "securitygroup"
+  location            = azurerm_resource_group.testrg.location
+  resource_group_name = azurerm_resource_group.testrg.name
+
+  security_rule {
+    name                       = "AllowSSH"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "AllowHTTPS"
+    priority                   = 200
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "443"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "AllowHTTP"
+    priority                   = 300
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_network_interface" "testNIC" {
+  name                = "test-nic"
+  location            = azurerm_resource_group.testrg.location
+  resource_group_name = azurerm_resource_group.testrg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.private_subnet_1.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "testVM" {
+  name                = "test-virtual-machine"
+  resource_group_name = azurerm_resource_group.testrg.name
+  location            = azurerm_resource_group.testrg.location
+  size                = "Standard_F2"
+  admin_username      = "adminuser"
+  network_interface_ids = [
+    azurerm_network_interface.testNIC.id,
+  ]
+
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = file ("accesskeys.pub")
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+  
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+}
